56e5547e82fcb7cc69a90fd4c96ed67f84942350
doublegunz <gungunpriatna02@gmail.com>
2017-11-19 06:25:27 +0700

    delete unused file

--- vw_footer.php
+++
@@ -1,8 +0,0 @@
-<!-- Footer -->
-	<p class="footer">
-	Aplikasi pembayaran SPP dan DSP - Pelatihan KP 2017<br />
-	Halaman ini dimuat selama <strong><?php echo microtime(); ?></strong> detik
-	</p>
-</div>
-</body>
-</html>
